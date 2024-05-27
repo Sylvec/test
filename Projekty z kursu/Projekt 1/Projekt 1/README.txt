669ee5bdb255abf607a0d98791da699c22656ad0
Sylwek P. <you@example.com>
2024-05-27 12:14:08 +0200

    added h1

---
+++ Projekty z kursu/Projekt 1/Projekt 1/README.txt
@@ -0,0 +1,3 @@
+Nie zapomnijcie dodać swojego linku do Font Awesome! :)
+
+Jeśli ikony nie działają, to właśnie dlatego, że swój kit już usunąłem. ;) 
---
+++ js/script.js
@@ -0,0 +1,27 @@
+// document.addEventListener('DOMContentLoaded', function () {
+// 	const nav = document.querySelector('.navbar');
+
+// 	function addShadow() {
+// 		if (window.scrollY >= 300) {
+// 			nav.classList.add('shadow-bg');
+// 		} else {
+// 			nav.classList.remove('shadow-bg');
+// 		}
+// 	}
+
+// 	window.addEventListener('scroll', addShadow);
+// });
+
+document.addEventListener('DOMContentLoaded', function () {
+	const nav = document.querySelector('.navbar');
+
+	function addShadow() {
+		if (window.scrollY >= 200) {
+			nav.classList.add('shadow-bg');
+		} else {
+			nav.classList.remove('bg-shadow');
+		}
+	}
+
+	window.addEventListener('scroll', addShadow);
+});
